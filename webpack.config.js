d94dc666d6dc17984f7540a67721d0e4dd2fa990
Tom <tom.cragg1@gmail.com>
2020-01-25 13:49:39 +0100

    added mixins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -14,9 +15,13 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
         contentBase: path.join(__dirname, 'app'),
         hot: true,
-        port: 3000
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
     module: {
